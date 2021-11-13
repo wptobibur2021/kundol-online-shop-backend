8d403b7dfd07ffe96eafbc4deec901b547cf428a
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2021-11-13 11:58:04 +0600

    some chnage

--- index.js
+++ index.js
@@ -155,6 +155,12 @@ async function kundolDb (){
             const result = await collectionOrder.deleteOne(query)
             await res.json(result)
         })
+        app.delete('/api/product/remove/:id', async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: objectId(id)}
+            const result = await collectionProducts.deleteOne(query)
+            await res.json(result)
+        })
     }finally {
 
     }
